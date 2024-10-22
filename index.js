5cf85c80ff67b637f24ead303864b5c1480e4724
likerk <nagorniykirill@gmail.com>
2024-10-22 13:45:44 +0400

    refactoring code and add media reaquest

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
-const game = document.querySelector(".game")
+const game = document.querySelector(".game");
 
-for (let i = 0; i < 9; i += 1){
-    const square = document.createElement("div")
-    square.classList.add("square")
-    game.append(square)
+const state = {
+  squares: [
+    {text: '', isClicked: false },
+    {text: '', isClicked: false },
+    {text: '', isClicked: false },
+    {text: '', isClicked: false },
+    {text: '', isClicked: false },
+    {text: '', isClicked: false },
+    {text: '', isClicked: false },
+    {text: '', isClicked: false },
+    {text: '', isClicked: false },
+  ]
 }
+// for (let i = 0; i < 9; i += 1){
+//     const square = document.createElement("div")
+//     square.classList.add("square")
+//     game.append(square)
+// }
+state.squares.forEach(square => {
+  const div = document.createElement("div");
+  div.classList.add("square");
+  div.textContent = square.text;
+  game.append(div);
+})
